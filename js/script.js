2039335ff02693cbe811d613fe074ff50ca10203
Samantha Keza <samanthakeza2000@gmail.com>
2019-07-26 09:48:13 +0200

    adjusting the textarea

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $("#png").click(function(){
+        $(".paragraph-showing").toggle();
+        $("#two").toggle();
+   
+    });
+});
+$(document).ready(function(){
+    $("#jnp").click(function(){
+        $(".p-showing").toggle();
+        $("#three").toggle();
+   
+    });
+});
+$(document).ready(function(){
+    $("#ing").click(function(){
+        $(".p3-showing").toggle();
+        $("#four").toggle();
+   
+    });
+});
